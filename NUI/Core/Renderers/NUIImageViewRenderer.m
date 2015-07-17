7a1d71660f8f9ef5c1fbde2f3867d5fdbd205489
Victor <victor.rodriguez@odigeo.com>
2015-07-17 14:10:53 +0200

    NUIImageViewRender update
    Call to nuiViewRender first to render all the common properties

--- NUI/Core/Renderers/NUIImageViewRenderer.m
+++ NUI/Core/Renderers/NUIImageViewRenderer.m
@@ -12,31 +12,18 @@
 
 + (void)renderImageView:(UIImageView*)imageView withClass:(NSString*)className
 {
-    [self render:imageView withClass:className withSuffix:@""];
-}
-
-+ (void)render:(UIImageView*)imageView withClass:(NSString*)className withSuffix:(NSString*)suffix
-{
-    if (![suffix isEqualToString:@""]) {
-        className = [NSString stringWithFormat:@"%@%@", className, suffix];
-    }
+    [NUIViewRenderer render:imageView withClass:className];
     
     if ([NUISettings hasProperty:@"tint-color" withClass:className]) {
+        
         imageView.tintColor = [NUISettings getColor:@"tint-color" withClass:className];
     }
     
     if ([NUISettings hasProperty:@"image-name" withClass:className]) {
+        
         UIImage *image = [NUISettings getImage:@"image-name" withClass:className];
         image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
         [imageView setImage:image];
-    }
-    
-    if ([NUISettings hasProperty:@"border-color" withClass:className]) {
-        [imageView.layer setBorderColor:[[NUISettings getColor:@"border-color" withClass:className] CGColor]];
-    }
-    
-    if ([NUISettings hasProperty:@"border-width" withClass:className]) {
-        [imageView.layer setBorderWidth:[NUISettings getFloat:@"border-width" withClass:className]];
     }
 }
 
